15bbeec9588abb4ffa1d7f45d49e42510e9f1385
h4yleysh4rpe <sharpeh42@gmail.com>
2025-06-21 16:27:58 +1200

    add blob

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Header from "./components/Header.jsx";
 import HeroSection from "./components/HeroSection.jsx";
+import MouseBlob from "./components/MouseBlob.jsx";
 
 function App() {
   return (
     <div className="bg-bgblue min-h-full text-gold p-6 text-3xl">
+      <MouseBlob />
       <Header />
       <div className="h-48"></div>
       <HeroSection />
