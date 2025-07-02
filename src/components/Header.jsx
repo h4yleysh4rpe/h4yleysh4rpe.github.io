159cff96108eb353a815b0edca689f95da15bd00
h4yleysh4rpe <sharpeh42@gmail.com>
2025-07-03 10:16:32 +1200

    route between pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <header className="bg-headerblue font-roboto px-8 text-navy h-28 flex flex-row items-center justify-between p-4 shadow-md rounded-xl">
       <h1 className="text-3xl text-shadow-bottom-navy font-judson">
         Hayley Sharpe
       </h1>
       <div className="flex text-xl flex-row items-center gap-8">
-        <div className="bg-buttonblue shadow-lg shadow-(color:navy) transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-hoverbuttonblue rounded-lg px-5 py-4">
-          Hello1
+        <div
+          onClick={() => navigate("/about")}
+          className="bg-buttonblue shadow-lg shadow-(color:navy) transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-hoverbuttonblue rounded-lg px-5 py-4"
+        >
+          About
         </div>
-        <div className="bg-buttonblue shadow-lg shadow-(color:navy) transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-hoverbuttonblue rounded-lg px-5 py-4">
-          Hell2eiqwrf
+        <div
+          onClick={() => navigate("/about")}
+          className="bg-buttonblue shadow-lg shadow-(color:navy) transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-hoverbuttonblue rounded-lg px-5 py-4"
+        >
+          Portfolio
         </div>
         <div className="bg-buttonblue shadow-lg shadow-(color:navy) transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-hoverbuttonblue rounded-lg px-5 py-4">
           NFkjsef
