3948f93dd9da95d25c4194ed3595d40078a5dad1
h4yleysh4rpe <sharpeh42@gmail.com>
2025-07-19 17:19:52 +1200

    add experience button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
       >
         Hayley Sharpe
       </h1>
-      <div className="flex text-xl flex-row items-center gap-8">
+      <div className="flex text-xl flex-row items-center gap-6">
         <div
           onClick={() => navigate("/about")}
           className="bg-buttonblue shadow-lg shadow-(color:navy) transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-hoverbuttonblue rounded-lg px-5 py-4"
@@ -18,10 +18,16 @@ export default function Header() {
           About
         </div>
         <div
-          onClick={() => navigate("/about")}
+          onClick={() => navigate("/portfolio")}
           className="bg-buttonblue shadow-lg shadow-(color:navy) transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-hoverbuttonblue rounded-lg px-5 py-4"
         >
           Portfolio
+        </div>
+        <div
+          onClick={() => navigate("/experience")}
+          className="bg-buttonblue shadow-lg shadow-(color:navy) transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-hoverbuttonblue rounded-lg px-5 py-4"
+        >
+          Experience
         </div>
       </div>
     </header>
