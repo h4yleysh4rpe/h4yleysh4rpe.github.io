fcaa4b64bdcc69a693ba9acce7151a011705f809
h4yleysh4rpe <sharpeh42@gmail.com>
2025-07-19 22:02:57 +1200

    add basic portfolio page structure

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,5 +1,19 @@
 import Timeline from "../components/Timeline";
 
 export default function PortfolioPage() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <div className="flex flex-row ml-6 gap-4">
+        <div className="flex items-center justify-center bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter">
+          Academic Transcript
+        </div>
+        <div className="flex items-center justify-center bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter">
+          CV
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="text-navy">Projects</div>
+      </div>
+    </div>
+  );
 }
