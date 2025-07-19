eb15963136f6a38d938b2acd1012656b8c9bbaa4
h4yleysh4rpe <sharpeh42@gmail.com>
2025-07-19 17:15:55 +1200

    add experience page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import PortfolioPage from "./pages/PortfolioPage";
+import ExperiencePage from "./pages/ExperiencePage.js";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/experience" element={<ExperiencePage />} />
       </Routes>
     </Router>
   );
