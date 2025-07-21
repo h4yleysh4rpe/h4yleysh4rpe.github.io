e56bdc49f42a8cf0cfe440e5d11d7e8fb6971b48
h4yleysh4rpe <sharpeh42@gmail.com>
2025-07-21 15:35:26 +1200

    update button styling on portfolio page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,5 @@
+import { FaArrowRight } from "react-icons/fa";
+
 export default function Projects() {
   const projectData = [
     {
@@ -24,17 +26,18 @@ export default function Projects() {
 
   return (
     <div>
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col bg-headerblue w-[97%] py-6 rounded-xl gap-4">
+      <div className="flex flex-col items-center font-inter">
+        <div className="flex flex-col bg-headerblue w-[97%] pt-6 pb-8 rounded-xl gap-4">
           <div className="text-navy ml-10 mb-3 mt-3 text-3xl">Projects</div>
           {projectData.map((project, index) => (
-            <div className="bg-headerblue shadow-lg shadow-(color:navy) mx-10 rounded-lg px-8 py-6">
+            <div className="bg-buttonblue shadow-lg shadow-(color:navy) mx-10 rounded-lg px-8 py-6">
               <div className="text-navy text-3xl mb-3">{project.title}</div>
               <div className="text-navy text-lg mb-3">
                 {project.description}
               </div>
-              <div className="bg-buttonblue w-32 cursor-pointer shadow-lg shadow-(color:navy) transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-hoverbuttonblue rounded-lg px-5 py-4">
-                Check it out
+              <div className="flex relative hover:pr-10 group items-center justify-center shadow-lg hover:bg-lightnavy text-bgblue transition-all duration-500 hover:scale-110 bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter">
+                Check it out{" "}
+                <FaArrowRight className="ml-2 group-hover:opacity-100 opacity-0 absolute right-3.5 text-bgblue group-hover:text-fadedgold transition-all duration-300 scale-0 group-hover:scale-100" />
               </div>
             </div>
           ))}
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,19 +1,22 @@
 import Projects from "../components/Projects";
 import Timeline from "../components/Timeline";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function PortfolioPage() {
   return (
     <div>
       <div className="flex flex-row ml-6 gap-4">
-        <div className="flex items-center justify-center bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter">
-          Academic Transcript
+        <div className="flex relative hover:pr-10 group items-center justify-center shadow-lg hover:bg-lightnavy text-bgblue transition-all duration-500 hover:scale-110 bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter">
+          Academic Transcript{" "}
+          <FaArrowRight className="ml-2 group-hover:opacity-100 opacity-0 absolute right-3.5 text-bgblue group-hover:text-fadedgold transition-all duration-300 scale-0 group-hover:scale-100" />
         </div>
-        <div className="flex items-center justify-center bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter">
-          CV
+        <div className="flex relative hover:pr-10 group items-center justify-center shadow-lg hover:bg-lightnavy text-bgblue transition-all duration-500 hover:scale-110 bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter">
+          CV{" "}
+          <FaArrowRight className="ml-2 group-hover:opacity-100 opacity-0 absolute right-3.5 text-bgblue group-hover:text-fadedgold transition-all duration-300 scale-0 group-hover:scale-100" />
         </div>
       </div>
       <div className="flex flex-col">
-        <div className="text-navy">Projects</div>
+        <div className="mb-8"></div>
         <Projects />
       </div>
     </div>
