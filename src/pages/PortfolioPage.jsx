1229601f0b89217ec9c2ece96f717a53426f9861
h4yleysh4rpe <sharpeh42@gmail.com>
2025-07-22 14:23:27 +1200

    add pdfs to website

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -6,11 +6,17 @@ export default function PortfolioPage() {
   return (
     <div>
       <div className="flex flex-row ml-6 gap-4">
-        <div className="flex relative hover:pr-10 group items-center justify-center shadow-lg hover:bg-lightnavy text-bgblue transition-all duration-500 hover:scale-110 bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter">
+        <div
+          className="flex relative hover:pr-10 group items-center justify-center shadow-lg hover:bg-lightnavy text-bgblue transition-all duration-500 hover:scale-110 bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter"
+          onClick={() => window.open("/transcript.pdf", "_blank")}
+        >
           Academic Transcript{" "}
           <FaArrowRight className="ml-2 group-hover:opacity-100 opacity-0 absolute right-3.5 text-bgblue group-hover:text-fadedgold transition-all duration-300 scale-0 group-hover:scale-100" />
         </div>
-        <div className="flex relative hover:pr-10 group items-center justify-center shadow-lg hover:bg-lightnavy text-bgblue transition-all duration-500 hover:scale-110 bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter">
+        <div
+          className="flex relative hover:pr-10 group items-center justify-center shadow-lg hover:bg-lightnavy text-bgblue transition-all duration-500 hover:scale-110 bg-navy rounded-3xl w-fit px-4 h-fit py-3 text-xl font-inter"
+          onClick={() => window.open("/resume.pdf", "_blank")}
+        >
           CV{" "}
           <FaArrowRight className="ml-2 group-hover:opacity-100 opacity-0 absolute right-3.5 text-bgblue group-hover:text-fadedgold transition-all duration-300 scale-0 group-hover:scale-100" />
         </div>
